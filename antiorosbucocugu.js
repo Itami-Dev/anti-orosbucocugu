92853ad01cefac17ee41e8ba410f30194c96fcb2
Itami-Dev <68259202+Itami-Dev@users.noreply.github.com>
2021-03-29 01:06:22 +0300

    Create antiorosbucocugu.js

---
+++ antiorosbucocugu.js
@@ -0,0 +1,17 @@
+include "discord";
+include "orosbucocugutarayicisi";
+
+
+
+
+Sunucunuza SæM#8216 isimli kişiyi almayın. // RECODED BY ITAMI
+
+
+break;
+
+
+
+
+
+
+// KULLANIRKEN LUTFEN KAYNAK VERINIZ!
